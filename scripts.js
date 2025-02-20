669134a19baeb4f4de9f43d7c260a58c7f704d51
dedisuartana <dedisuartana12@gmail.com>
2025-02-20 12:05:04 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+document.getElementById('reportType').addEventListener('change', function () {
+    const selectedValue = this.value;
+    const reportCards = document.querySelectorAll('.report-card');
+
+    reportCards.forEach(card => {
+        if (selectedValue === 'all' || card.classList.contains(selectedValue)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
+document.querySelector('.report-card.hardware button').addEventListener('click', function () {
+    window.location.href = 'https://docs.google.com/spreadsheets/d/19FCKjhrsuxiTd-2U_yPqr9rG_Hs35sBy4K1fMTQbgCU/edit?gid=0#gid=0'; // Ganti dengan halaman tujuan
+});
