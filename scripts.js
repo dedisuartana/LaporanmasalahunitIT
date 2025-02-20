a630ff3524ad1dce3e3711b05226ff7c6155854f
dedisuartana <dedisuartana12@gmail.com>
2025-02-20 12:56:35 +0800

    scripts.js

--- scripts.js
+++ scripts.js
@@ -13,3 +13,6 @@ document.getElementById('reportType').addEventListener('change', function () {
 document.querySelector('.report-card.hardware button').addEventListener('click', function () {
     window.location.href = 'https://docs.google.com/spreadsheets/d/19FCKjhrsuxiTd-2U_yPqr9rG_Hs35sBy4K1fMTQbgCU/edit?gid=0#gid=0'; // Ganti dengan halaman tujuan
 });
+document.querySelector('.report-card.software button').addEventListener('click', function () {
+    window.location.href = 'https://docs.google.com/spreadsheets/d/16964zopoGN7ExkqRJeMpgss8rw2oD7smyKOvrJR-rmI/edit?usp=sharing'; // Ganti dengan halaman tujuan
+});
