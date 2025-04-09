3303e231ab54b9d4ea53696562045df57966375d
Dedi Suartana <dedisuartana12@gmail.com>
2025-04-09 10:41:17 +0700

    scripts.js

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ document.querySelector('.report-card.hardware button').addEventListener('click',
     window.location.href = 'https://docs.google.com/spreadsheets/d/19FCKjhrsuxiTd-2U_yPqr9rG_Hs35sBy4K1fMTQbgCU/edit?gid=0#gid=0'; // Ganti dengan halaman tujuan
 });
 document.querySelector('.report-card.software button').addEventListener('click', function () {
-    window.location.href = 'https://docs.google.com/spreadsheets/d/16964zopoGN7ExkqRJeMpgss8rw2oD7smyKOvrJR-rmI/edit?usp=sharing'; // Ganti dengan halaman tujuan
+    window.location.href = 'bingung.html.html'; // Ganti dengan halaman tujuan
 });
 document.querySelector('.report-card.report button').addEventListener('click', function () {
     window.location.href = 'http://192.168.75.3:2500/system/report'; // Ganti dengan halaman tujuan
